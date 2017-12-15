3edea9ac943b132c52b34d2fa155d74f77d6da81
Joren <jorenjamar@hotmail.com>
2017-12-15 13:55:52 +0100

    basis en startscherm gemaakt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { JorenComponent } from './joren/joren.component';
+import { BasComponent } from './bas/bas.component';
+import { WcComponent } from './wc/wc.component';
+import { VervoerComponent } from './vervoer/vervoer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    JorenComponent,
+    BasComponent,
+    WcComponent,
+    VervoerComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {path: "home", component : HomeComponent},
+      {path: "wc", component : WcComponent},
+      {path: "vervoer", component : VervoerComponent},
+      {path: "", redirectTo: "home", pathMatch: "full"},
+    ], {useHash: true}),
     MDBBootstrapModule.forRoot()
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
---
+++ src/app/bas/bas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "app-bas",
+    templateUrl : "bas.component.html"
+})
+
+export class BasComponent{
+
+}
